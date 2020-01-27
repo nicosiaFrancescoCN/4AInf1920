36e42e95dcca5973f13272516ac6b5bebab6d49f
nicosiaFrancescoCN <57175153+nicosiaFrancescoCN@users.noreply.github.com>
2020-01-27 12:10:01 +0100

    Add files via upload

---
+++ 023InterfaccePoligoni/src/it/itis/cuneo/Rettangolo.java
@@ -0,0 +1,56 @@
+package it.itis.cuneo;
+
+public class Rettangolo implements Poligono{
+
+    private float lato1;
+    private float lato2;
+    private float lato3;
+    private float lato4;
+
+    public Rettangolo() {
+    }
+
+    public Rettangolo(float lato1, float lato2, float lato3, float lato4) {
+        this.lato1 = lato1;
+        this.lato2 = lato2;
+        this.lato3 = lato3;
+        this.lato4 = lato4;
+    }
+
+    public float getLato1() {
+        return lato1;
+    }
+
+    public void setLato1(float lato1) {
+        this.lato1 = lato1;
+    }
+
+    public float getLato2() {
+        return lato2;
+    }
+
+    public void setLato2(float lato2) {
+        this.lato2 = lato2;
+    }
+
+    public float getLato3() {
+        return lato3;
+    }
+
+    public void setLato3(float lato3) {
+        this.lato3 = lato3;
+    }
+
+    public float getLato4() {
+        return lato4;
+    }
+
+    public void setLato4(float lato4) {
+        this.lato4 = lato4;
+    }
+
+    @Override
+    public double perimetro() {
+        return(this.getLato1() + this.getLato2() + this.getLato3() + this.getLato4());
+    }
+}
